e3d3b16abf482535779aea988357969026ab1aee
vsingh0809 <166600572+vsingh0809@users.noreply.github.com>
2024-04-10 14:52:37 +0530

    Add files via upload

---
+++ aray.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+int size;
+cout<<"give a size of arr"<<endl;
+cin>>size;	
+int arr[size]={10,20,21,524,54,54,2,542,452,1,5};
+
+for(int i=0;i<size;i++)  {
+
+cout<<"array display"<<arr[i]<<endl;
+}
+	
+
+	
+}
+
