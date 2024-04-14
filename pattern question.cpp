aac4b2db919334f34e3e93fcfecbdbdac94af182
vsingh0809 <166600572+vsingh0809@users.noreply.github.com>
2024-04-14 14:02:44 +0530

    Update pattern question.cpp

--- pattern question.cpp
+++ pattern question.cpp
@@ -1,23 +1,13 @@
 #include<iostream>
 using namespace std;
-
-
 int main(){
 	for(int i =1;i<=4;i++){
-		for (int j=1;j<=5;j++){
+	for (int j=1;j<=5;j++){
 			
-			cout<<"*";
-		}
-		
-		
-		
-		cout<<""<<endl;
-		
-		
-		
-		
+          cout<<"*";
 	}
-	
-	
-	
-}
+			
+         cout<<""<<endl;
+			
+	}
+	}
